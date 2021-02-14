2b0f2cdcf84a3ccfdda581e741167126606d5268
femarleycode <femarleycode@gmail.com>
2021-02-14 16:19:44 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using static MongoDBDemo.AddressModel;
+
+namespace MongoDBDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MongoCRUD db = new MongoCRUD("AddressBook");
+
+			PersonModel person = new PersonModel
+			{
+				FirstName = "Joe",
+				LastName = "Smith",
+				PrimaryAddress = new AddressModel
+				{
+					StreetAddress = "101 Oak Street",
+					City = "Scranton",
+					State = "PA",
+					ZipCode = "18512"
+				}
+			};
+
+			db.InsertRecord("Users", person);
+			Console.ReadLine();
+		}
+	}
+
+	public class PersonModel {
+		[BsonId] // id
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public AddressModel PrimaryAddress { get; set; }
+	}
+
+	public class AddressModel
+	{
+		public string StreetAddress { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string ZipCode { get; set; }
+	}
+		public class MongoCRUD
+	{
+		private IMongoDatabase db;
+
+		public MongoCRUD(string database)
+		{
+			var client = new MongoClient();
+			db = client.GetDatabase(database);
+		}
+
+		public void InsertRecord<T>(string table, T record) {
+			var collection = db.GetCollection<T>(table);
+			collection.InsertOne(record);
+		}
+
+		public List<T> LoadRecord<T>(string table)
+		{
+			var collection = db.GetCollection<T>(table);
+
+			return collection.Find(new BsonDocument()).ToList();
+		}
+	}
+}
